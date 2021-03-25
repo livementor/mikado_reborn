16159c47daaa61dcfcf3702eb80ddf2bc1b87443
Alexandre <alexandr.barbier@gmail.com>
2021-03-25 18:11:49 +0100

    initialisation

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+
+    },
+    opacity: {
+
+    },
+    spacing: {},
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
