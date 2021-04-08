eb5d62767858f1c367c5f953879fa95b00d02210
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2021-04-08 09:15:23 +0200

    feat(icon): add icon component (#3)

---
+++ packages/mikado_reborn/src/components/Icon/Icon.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="mkr__icon" :class="classNames"/>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Icon extends Vue {
+  @Prop() private name!: string;
+
+  @Prop() private color!: string;
+
+  get classNames() : string[] {
+    const classes = [];
+    if (this.color) classes.push(this.color);
+    if (this.name) classes.push(`icon-${this.name}`);
+    return classes;
+  }
+}
+</script>
+<style src="./Icon.scss" lang="scss"></style>
