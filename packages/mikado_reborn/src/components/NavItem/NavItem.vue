f00fce6bc125536f36269e7a33aea3a1b3e9d7c5
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2021-04-09 17:30:48 +0200

    feat(nav-item): add nav-item component (#11)
    * feat(nav-item): add nav-item component
    
    * fix(nav-item): change story name

---
+++ packages/mikado_reborn/src/components/NavItem/NavItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li :class="classes">
+    <slot />
+  </li>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+@Component
+export default class NavItem extends Vue {
+  @Prop({ type: Boolean })
+  active!: boolean;
+
+  get classes(): (string | {[className: string]: boolean;})[] {
+    return [
+      'mkr__nav-item',
+      {
+        'mkr__nav-item--active': this.active,
+      },
+    ];
+  }
+
+  /**
+   * Passthrough click event
+   */
+  emitClick(event: MouseEvent): void {
+    this.$emit('click', event);
+  }
+}
+</script>
+<style src="./NavItem.scss" lang="scss"></style>
---
+++ packages/mikado_reborn/src/components/NavItem/index.ts
@@ -0,0 +1,5 @@
+import MkrNavItem from './NavItem.vue';
+import MkrNavItemGroup from './NavItemGroup.vue';
+
+export { MkrNavItem, MkrNavItemGroup };
+export default MkrNavItem;
