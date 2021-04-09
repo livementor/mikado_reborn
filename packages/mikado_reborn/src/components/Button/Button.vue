bd93e16e202c90cae0ab3a4f9c4a72f87d43bee2
Alexandre <alexandr.barbier@gmail.com>
2021-04-09 10:58:42 +0200

    add click

--- packages/mikado_reborn/src/components/Button/Button.vue
+++ packages/mikado_reborn/src/components/Button/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <button :disabled="disabled" :class="['mkr__button', type, size]">
+  <button :disabled="disabled" :class="['mkr__button', type, size]" @click="click">
     <mkr-icon v-if="icon" :name="icon" />
     <slot />
   </button>
@@ -10,14 +10,21 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class Button extends Vue {
-  @Prop({ default: 'primary' }) type!: string;
+  @Prop({ default: 'primary' })
+  type!: string;
 
-  @Prop({ default: false }) disabled: boolean;
+  @Prop({ default: false })
+  disabled: boolean;
 
-  @Prop() icon?: string;
+  @Prop()
+  icon?: string;
 
   @Prop({validator: (size) => {return ['small', 'medium', 'large'].includes(size)}})
   size: string;
+
+  click(event) {
+    this.$emit('click', event)
+  }
 }
 </script>
 <style src="./Button.scss" lang="scss"></style>
