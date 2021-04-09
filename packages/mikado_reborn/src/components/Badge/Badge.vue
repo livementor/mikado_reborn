7ac2e9707cecf84aecd50d5d9827d48984617220
Alexandre <alexandr.barbier@gmail.com>
2021-04-09 08:20:18 +0200

    create badge component

---
+++ packages/mikado_reborn/src/components/Badge/Badge.vue
@@ -0,0 +1,15 @@
+<template>
+  <span :class="['mkr__badge', type]"><slot/></span>
+</template>
+
+<script lang=ts>
+  import { Component, Vue, Prop } from 'vue-property-decorator';
+
+  @Component
+  export default class Badge extends Vue {
+    @Prop({default: 'warning', validator: (type) => {return ['danger', 'success'].includes(type)}})
+    type!: string
+  }
+</script>
+
+<style src="./Badge.scss" lang=scss></style>
