95c1d77620d2a3fe85759ce8e3bb2aa6e414c207
Alexandre <alexandr.barbier@gmail.com>
2021-04-09 18:14:04 +0200

    add colors

--- packages/mikado_reborn/src/utils/colors.js
+++ packages/mikado_reborn/src/utils/colors.js
@@ -23,4 +23,29 @@ const colors = [
   'warning-light',
 ];
 
+export const colorMap = {
+  primary: ['primary-primary', 'rgba(255, 215, 122, 1)'],
+  'primary-light': ['primary-primary-l', 'rgba(255, 247, 228, 1)'],
+  'primary-dark': ['primary-primary-d', 'rgba(136, 71, 7, 1)'],
+  secondary: ['primary-secondary', 'rgba(7, 146, 121, 1)'],
+  'secondary-light': ['primary-secondary-l', 'rgba(231, 254, 250, 1)'],
+  'secondary-dark': ['primary-secondary-d', 'rgba(3, 64, 53, 1)'],
+  accent: ['accent-accent', 'rgba(243, 189, 163, 1)'],
+  'accent-light': ['accent-accent-l', 'rgba(252, 239, 227, 1)'],
+  white: ['neutral-white', 'rgba(255, 255, 255, 1)'],
+  neutral: ['neutral-neutral', 'rgba(40, 56, 72, 1)'],
+  'neutral-light': ['neutral-neutral-l', 'rgba(239, 241, 241, 1)'],
+  'neutral-dark': ['neutral-neutral-d', 'rgba(13, 22, 28, 1)'],
+  'neutral-80': ['neutral-neutral-80', 'rgba(40, 56, 72, 0.8)'],
+  'neutral-60': ['neutral-neutral-60', 'rgba(40, 56, 72, 0.6)'],
+  'neutral-40': ['neutral-neutral-40', 'rgba(40, 56, 72, 0.4)'],
+  'neutral-20': ['neutral-neutral-20', 'rgba(40, 56, 72, 0.2)'],
+  success: ['alert-success', 'rgba(9, 124, 95, 1)'],
+  'success-light': ['alert-success-l', 'rgba(225, 245, 238, 1)'],
+  danger: ['alert-danger', 'rgba(191, 43, 13, 1)'],
+  'danger-light': ['alert-danger-l', 'rgba(245, 225, 225, 1)'],
+  warning: ['alert-warning', 'rgba(148, 99, 0, 1)'],
+  'warning-light': ['alert-warning-l', 'rgba(254, 241, 215, 1)'],
+};
+
 export default colors;
