e80f46f58fd6f99b3347cae6c86a1e497736a7b5
Guillaume Baechler <baechler.guillaume.pro@gmail.com>
2021-04-09 15:55:29 +0200

    feat(nav-item): add nav-item component

---
+++ packages/doc/src/stories/NavItem.stories.js
@@ -0,0 +1,47 @@
+import { MkrNavItem, MkrNavItemGroup } from "../../../mikado_reborn/src/components";
+
+export default {
+  title: 'Components/NavItem',
+  component: MkrNavItem,
+  subcomponents: { 'MkrNavItemGroup': MkrNavItemGroup },
+};
+
+const Template = (args) => ({
+  props: Object.keys(args),
+  template: `
+    <div class="primary-light-bg" style="padding:2rem;">
+      <!-- application links -->
+      <nav aria-label="application">
+        <mkr-nav-item-group>
+          <mkr-nav-item active>
+            <mkr-icon name="user"/>
+            <a href="#">Mon profil</a>
+          </mkr-nav-item>
+          <mkr-nav-item>
+            <mkr-icon name="briefcase"/>
+            <a href="#">Mon projet</a>
+          </mkr-nav-item>
+          <mkr-nav-item>
+            <mkr-icon name="cog"/>
+            <a href="#">Paramètres</a>
+          </mkr-nav-item>
+        </mkr-nav-item-group>
+      </nav>
+      <!-- external links -->
+      <mkr-nav-item-group style="padding-top:5rem;">
+        <mkr-nav-item>
+          <a href="#" target="_blank">Programme de fidélité</a>
+        </mkr-nav-item>
+        <mkr-nav-item>
+          <a href="#">Parrainage</a>
+        </mkr-nav-item>
+        <mkr-nav-item>
+          <a href="#">Codes promo</a>
+        </mkr-nav-item>
+      </mkr-nav-item-group>
+    </div>
+  `,
+});
+
+export const Nav = Template.bind({});
+Nav.args = {};
---
+++ packages/mikado_reborn/src/components/NavItem/NavItemGroup.vue
@@ -0,0 +1,13 @@
+<template>
+  <ul class="mkr__nav-item-group">
+    <slot />
+  </ul>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Nav extends Vue {}
+</script>
+<style src="./NavItemGroup.scss" lang="scss"></style>
