8520930e51bbe045488d8290aa6c907bf25ee2fd
Guillaume Baechler <baechler.guillaume.pro@gmail.com>
2021-04-14 14:24:33 +0200

    remove shadow rule

--- packages/mikado_reborn/.eslintrc.js
+++ packages/mikado_reborn/.eslintrc.js
@@ -14,8 +14,6 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": ["error"],
   },
   settings: {
     'import/resolver': {
