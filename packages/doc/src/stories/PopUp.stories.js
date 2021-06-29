1d83423f6a90b4cf8baa80cd4ff7b802c058e9bf
Alexandre <alexandr.barbier@gmail.com>
2021-06-29 18:26:13 +0200

    use backtick

--- packages/doc/src/stories/PopUp.stories.js
+++ packages/doc/src/stories/PopUp.stories.js
@@ -10,7 +10,16 @@ export default {
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  template: '<mkr-pop-up v-bind="$props"><template v-slot:anchor><mkr-contained-button>button</mkr-contained-button></template><mkr-card :elevated="true"><mkr-contained-button>test</mkr-contained-button></mkr-card></mkr-pop-up>',
+  template: `
+    <mkr-pop-up v-bind="$props">
+      <template v-slot:anchor>
+        <mkr-contained-button>button</mkr-contained-button>
+      </template>
+      <mkr-card :elevated="true">
+        <mkr-contained-button>test</mkr-contained-button>
+      </mkr-card>
+    </mkr-pop-up>
+  `,
 });
 
 export const PopUp = Template.bind({});
