ec57372509673df98f1eb7d0662fe45ff6c4ea8c
Guillaume Baechler <baechler.guillaume.pro@gmail.com>
2021-06-30 11:46:05 +0200

    feat(datatable): add datatable component

---
+++ packages/doc/src/stories/DataTable.stories.js
@@ -0,0 +1,44 @@
+import { MkrDataTable } from "../../../mikado_reborn/src/components";
+
+export default {
+  title: 'Components/DataTable',
+  component: MkrDataTable,
+  argTypes: {},
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  template: `
+    <mkr-data-table v-bind="$props" />`
+});
+
+export const DataTable = Template.bind({});
+DataTable.args = {
+  title: 'Formations',
+  columns: [
+    {
+      label: 'Début',
+      value: 'startDate',
+    },
+    {
+      label: 'Fin',
+      value: 'endDate',
+    },
+    {
+      label: 'Formation',
+      value: 'formation',
+    }
+  ],
+  items: [
+    {
+      startDate: '12/02/2019',
+      endDate: '13/05/2019',
+      formation: 'Nom de la formation'
+    },
+    {
+      startDate: '08/01/2020',
+      endDate: '12/04/2020',
+      formation: 'Nom de la formation'
+    }
+  ]
+};
