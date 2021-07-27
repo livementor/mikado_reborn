79a8275974f4b971bd9671c2ba8325ce782014bd
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2021-07-27 14:38:50 +0200

    feat(nav-item): add nav-item types (main, sub and sub-light)

--- packages/mikado_reborn/src/components/NavItem/NavItemGroup.vue
+++ packages/mikado_reborn/src/components/NavItem/NavItemGroup.vue
@@ -1,13 +1,25 @@
 <template>
-  <ul class="mkr__nav-item-group">
+  <ul class="mkr__nav-item-group" :class="`mkr__nav-item-group--${type}`">
     <slot />
   </ul>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop } from 'vue-property-decorator';
+
+export const NavItemTypes = {
+  main: 'main',
+  sub: 'sub',
+  'sub-light': 'light',
+};
 
 @Component
-export default class Nav extends Vue {}
+export default class NavItemGroup extends Vue {
+  @Prop({
+    default: NavItemTypes.main,
+    validator: (type) => Object.values(NavItemTypes).includes(type),
+  })
+  type!: string
+}
 </script>
 <style src="./NavItemGroup.scss" lang="scss"></style>
