77faa4198e3abd0817273385f28387f80c4760d1
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2021-07-27 17:32:15 +0200

    feat(nav-item): add styles for new types and refactor icon

--- packages/mikado_reborn/src/components/NavItem/NavItemGroup.vue
+++ packages/mikado_reborn/src/components/NavItem/NavItemGroup.vue
@@ -1,5 +1,11 @@
 <template>
-  <ul class="mkr__nav-item-group" :class="`mkr__nav-item-group--${type}`">
+  <ul
+    class="mkr__nav-item-group"
+    :class="[
+      `mkr__nav-item-group--${type}`,
+      { 'mkr__nav-item-group--top-nav': topNav },
+    ]"
+  >
     <slot />
   </ul>
 </template>
@@ -10,7 +16,7 @@ import { Component, Vue, Prop } from 'vue-property-decorator';
 export const NavItemTypes = {
   main: 'main',
   sub: 'sub',
-  'sub-light': 'light',
+  'sub-light': 'sub-light',
 };
 
 @Component
@@ -20,6 +26,9 @@ export default class NavItemGroup extends Vue {
     validator: (type) => Object.values(NavItemTypes).includes(type),
   })
   type!: string
+
+  @Prop({ type: Boolean, default: false })
+  topNav!: boolean;
 }
 </script>
 <style src="./NavItemGroup.scss" lang="scss"></style>
