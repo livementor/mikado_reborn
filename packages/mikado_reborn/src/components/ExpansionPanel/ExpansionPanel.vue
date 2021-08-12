1a28f31071f87b35a650e5b79a7b2613668334d2
Hervé <h.hochart29@gmail.com>
2021-08-12 17:48:29 +0200

    :fix(expansion-panel): send event on header click (#93)
    * fix(expansion-panel): send event on header click
    
    * fix(expansion-panel): use slot binding to pass toggle method
    
    fix(expansion-panel): add prop to hide the default arrow
    
    fix(expansion-panel): add prop to hide the default arrow
    
    fix(expansion-panel): add prop to hide the default arrow
    
    * fix(expansion-panel): use slot binding to enable custom activator

--- packages/mikado_reborn/src/components/ExpansionPanel/ExpansionPanel.vue
+++ packages/mikado_reborn/src/components/ExpansionPanel/ExpansionPanel.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="mkr__expansion-panel">
     <div class="mkr__expansion-panel__header">
-      <mkr-interactive-icon
-        class="mkr__expansion-panel__header-icon"
-        :name="expanded ? 'arrow-full-top' : 'arrow-full-bottom'"
-        color="neutral-60"
-        @click.prevent="togglePanel"
-      />
+      <div class="mkr__expansion-panel__header-activator">
+        <slot name="activator" v-bind="{ expanded, toggle: togglePanel }">
+          <mkr-interactive-icon
+            :name="expanded ? 'arrow-full-top' : 'arrow-full-bottom'"
+            color="neutral-60"
+            @click.prevent="togglePanel"
+          />
+        </slot>
+      </div>
       <div class="mkr__expansion-panel__header-title">
-        <slot name="header" v-bind="{ expanded }" />
+        <slot name="header" v-bind="{ expanded, toggle: togglePanel }" />
       </div>
     </div>
 
