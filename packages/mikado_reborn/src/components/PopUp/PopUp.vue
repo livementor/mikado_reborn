6b0c9d71ac507885f74a826578c9da87f7221e94
Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
2021-08-19 18:38:59 +0200

    feat(pop-up): add dismissable prop (#98)
    * feat(pop-up): add dismissable prop
    
    * feat(pop-up): add dismissable doc

--- packages/mikado_reborn/src/components/PopUp/PopUp.vue
+++ packages/mikado_reborn/src/components/PopUp/PopUp.vue
@@ -24,13 +24,24 @@ export default class PopUp extends Vue {
   @Prop({ type: String, default: 'auto' })
   placement!: Placement
 
+  @Prop({ type: Boolean, default: false })
+  readonly dismissable!: boolean;
+
   popperInstance: PopperInstance | null = null
 
-  @Watch('opened')
-  async handleOpening(newVal: boolean): Promise<void> {
-    if (newVal) {
+  @Watch('opened', { immediate: true })
+  async handleOpening(isOpened: boolean): Promise<void> {
+    if (isOpened) {
       await this.$nextTick();
       await this.popperInstance?.update();
+    }
+
+    if (this.dismissable) {
+      if (isOpened) {
+        this.initCloseEventListeners();
+      } else {
+        this.removeCloseEventListeners();
+      }
     }
   }
 
@@ -49,6 +60,25 @@ export default class PopUp extends Vue {
         },
       ],
     });
+  }
+
+  initCloseEventListeners(): void {
+    document.addEventListener('mousedown', this.onClickOutside);
+  }
+
+  removeCloseEventListeners(): void {
+    document.removeEventListener('mousedown', this.onClickOutside);
+  }
+
+  onClickOutside(event: MouseEvent): void {
+    const target = event.target as Node | null;
+    if (!target) {
+      return;
+    }
+    const isClickOutside = !(this.$el.contains(event.target as Node));
+    if (isClickOutside) {
+      this.$emit('close', false);
+    }
   }
 
   async handleButtonClick(): Promise<void> {
