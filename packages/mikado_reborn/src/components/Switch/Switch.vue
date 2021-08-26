0a08ea3926523c98c5bf1f8ca350a7f104eafc1b
Hervé <h.hochart29@gmail.com>
2021-08-26 15:02:08 +0200

    fix(switch): remove wrapper div

--- packages/mikado_reborn/src/components/Switch/Switch.vue
+++ packages/mikado_reborn/src/components/Switch/Switch.vue
@@ -1,17 +1,15 @@
 <template>
-  <div>
-    <span
-      class="mkr__switch"
-      :class="{ 'mkr__switch--active': value }"
-      role="checkbox"
-      :aria-checked="value"
-      tabindex="0"
-      @click="toggle"
-      @keydown.space.prevent="toggle"
-    >
-      <span class="mkr__switch__indicator" />
-    </span>
-  </div>
+  <span
+    class="mkr__switch"
+    :class="{ 'mkr__switch--active': value }"
+    role="checkbox"
+    :aria-checked="value"
+    tabindex="0"
+    @click="toggle"
+    @keydown.space.prevent="toggle"
+  >
+    <span class="mkr__switch__indicator" />
+  </span>
 </template>
 
 <script lang="ts">
