47e83ce816680e35d6d778f8d487e0501eea2c10
Hervé <h.hochart29@gmail.com>
2021-08-26 11:21:54 +0200

    Update packages/doc/src/stories/Switch.stories.js
    Co-authored-by: Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>

--- packages/doc/src/stories/Switch.stories.js
+++ packages/doc/src/stories/Switch.stories.js
@@ -3,14 +3,11 @@ import { MkrSwitch } from '../../../mikado_reborn/src/components';
 export default {
   title: 'Components/Switch',
   component: MkrSwitch,
-  argTypes: {},
 };
 
-const Template = (args, { argTypes }) => ({
-  props: Object.keys(argTypes),
+const Template = () => ({
   data: () => ({ model: false }),
   template: '<mkr-switch v-bind="$props" v-model="model" />',
 });
 
 export const Switch = Template.bind({});
-Switch.args = {};
