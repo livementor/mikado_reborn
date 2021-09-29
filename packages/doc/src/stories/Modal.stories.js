0ade23c5f99388563a941de1880bd899b86105e5
Hervé <h.hochart29@gmail.com>
2021-09-29 14:40:45 +0200

    feat(modal): add focus trap (#109)
    * feat(modal): add focus trap
    
    * fix(modal story): use mkr component

--- packages/doc/src/stories/Modal.stories.js
+++ packages/doc/src/stories/Modal.stories.js
@@ -1,5 +1,9 @@
-import { MkrModal } from "../../../mikado_reborn/src/components";
-import { sizes } from "../../../mikado_reborn/src/components/Modal/Modal.vue";
+import { MkrModal } from '../../../mikado_reborn/src/components';
+import { sizes } from '../../../mikado_reborn/src/components/Modal/Modal.vue';
+import { MkrContainedButton } from '../../../mikado_reborn/src/components';
+import { MkrTextarea } from '../../../mikado_reborn/src/components';
+import { MkrTextfield } from '../../../mikado_reborn/src/components';
+import { MkrDropdown } from '../../../mikado_reborn/src/components';
 
 export default {
   title: 'Components/Modal',
@@ -9,8 +13,8 @@ export default {
       options: Object.values(sizes),
       control: {
         type: 'select',
-      }
-    }
+      },
+    },
   },
 };
 
@@ -20,9 +24,9 @@ const ModalTemplate = (args, { argTypes }) => ({
     showModal: false,
   }),
   methods: {
-    open () {
-      this.showModal = true
-    }
+    open() {
+      this.showModal = true;
+    },
   },
   template: `
     <div>
@@ -41,17 +45,37 @@ const ModalWithOverlayTemplate = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
   data: () => ({
     showModal: false,
+    selectModel: 'option1',
   }),
   methods: {
-    open () {
-      this.showModal = true
-    }
+    open() {
+      this.showModal = true;
+    },
   },
   template: `
     <div>
       <mkr-overlay :opened="showModal" />
       <mkr-modal v-bind="$props" v-model="showModal">
-        <span>Hello World</span>
+      <div style="display: flex; flex-direction: column">
+          <h1>Form</h1>
+
+          <div style="margin: 15px 0;">
+            <label>Input label</label>
+            <mkr-textfield type="text" />
+          </div>
+
+          <div>
+            <label>Textarea label</label>
+            <mkr-textarea></mkr-textarea>
+          </div>
+
+          <div style="margin: 15px 0;">
+            <label>Select label</label>
+            <mkr-dropdown v-model="selectModel" :items="['option1', 'option2']" />
+          </div>
+
+          <mkr-contained-button>Button</mkr-contained-button>
+        </div>
       </mkr-modal>
       <mkr-contained-button @click="open">Open Modal</mkr-contained-button>
     </div>
@@ -63,5 +87,5 @@ ModalWithOverlay.args = {};
 
 export const UnclosableModal = ModalWithOverlayTemplate.bind({});
 UnclosableModal.args = {
-  closeable: false
+  closeable: false,
 };
--- packages/mikado_reborn/src/components/Modal/Modal.vue
+++ packages/mikado_reborn/src/components/Modal/Modal.vue
@@ -28,10 +28,15 @@
 
 <script lang="ts">
 import {
-  Vue, Component, Model, Prop, Watch,
+  Vue,
+  Component,
+  Model,
+  Prop,
+  Watch,
 } from 'vue-property-decorator';
 import { MkrCard } from '../Card';
 import { MkrInteractiveIcon } from '../InteractiveIcon';
+import focusTrap from './focusTrap';
 
 export const sizes = {
   medium: 'medium',
@@ -63,8 +68,10 @@ export default class Modal extends Vue {
   @Prop({ type: String, default: null })
   readonly focusFirstSelector!: string;
 
+  focusTrapListenerCleanup: ReturnType<typeof focusTrap> = null;
+
   $refs!: {
-    modalContent: Element;
+    modalContent: HTMLDivElement;
   };
 
   @Watch('closeable')
@@ -78,13 +85,16 @@ export default class Modal extends Vue {
 
   @Watch('opened')
   async onOpenedChanged(isOpened: boolean): Promise<void> {
-    if (isOpened && this.focusFirstSelector) {
+    if (isOpened) {
       await this.$nextTick();
+      const modalRef = this.$refs.modalContent;
 
-      const ref = this.$refs.modalContent.querySelector(this.focusFirstSelector) as HTMLElement;
-      if (ref) {
-        ref.focus();
-      }
+      this.focusTrapListenerCleanup = focusTrap({
+        el: modalRef,
+        focusElement: modalRef.querySelector<HTMLElement>(this.focusFirstSelector),
+      });
+    } else if (this.focusTrapListenerCleanup) {
+      this.focusTrapListenerCleanup();
     }
 
     if (!this.closeable) return;
