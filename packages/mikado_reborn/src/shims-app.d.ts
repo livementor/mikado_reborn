8aec46a6b69f1b311304a92f227859648ae765ad
Mazel <mazuel.loic@gmail.com>
2021-11-03 14:04:09 +0100

    chore(lib|doc): update dependencies & adding makefile (#125)
    * fix(doc): typo of component name into import from textfield story
    
    * chore(doc|lib): update dependencies
    
    * fix(lib): disable new vue eslint rule for Card component
    
    * refactor(lib): remove unused dependency import from declaration file
    
    * feat(root): add Makefile
    
    * Add helpers to makefile
    
    Co-authored-by: Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
    
    * fix(lib): Remove useless disable rule of eslint
    
    Co-authored-by: Alexandre <alexandr.barbier@gmail.com>
    
    Co-authored-by: Guillaume BAECHLER <baechler.guillaume.pro@gmail.com>
    Co-authored-by: Alexandre <alexandr.barbier@gmail.com>

--- packages/mikado_reborn/src/shims-app.d.ts
+++ packages/mikado_reborn/src/shims-app.d.ts
@@ -1,9 +1,7 @@
-/* eslint-disable no-shadow */
-import Vue from 'vue';
 import App from './components/App';
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $app: App
+    $app: App;
   }
 }
