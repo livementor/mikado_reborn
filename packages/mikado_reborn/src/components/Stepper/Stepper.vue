5fc92ffb38e20b1a3eb3aa49f0ec993dd3cace49
leobernier <33323385+leobernier@users.noreply.github.com>
2023-10-18 16:54:32 +0200

    feat: stepper component (#170)

---
+++ packages/mikado_reborn/src/components/Stepper/Stepper.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <div class="mkr__stepper">
+      <!-- Header -->
+      <div class="mkr__stepper-header">
+        <template v-for="({ type, label }, i) in itemsAsObject">
+          <!-- Header Divider -->
+          <div v-if="i !== 0" :key="i" class="mkr__stepper-header__divider" />
+          <!-- Header Item -->
+          <div
+            :key="label"
+            class="mkr__stepper-header__item"
+            :class="{ 'current': step === i + 1 }"
+          >
+          <StepperIcon
+            v-if="step > i + 1 || type !== 'default'"
+            :type="type"
+          />
+          <div v-else class="mkr__stepper-header__item-number">
+            {{ i + 1 }}
+          </div>
+          <div class="mkr__stepper-header__item-title">
+            {{ label }}
+          </div>
+        </div>
+      </template>
+    </div>
+  </div>
+  <!-- Body -->
+  <div v-for="i in items.length" :key="i" class="mkr__stepper--body">
+    <slot v-if="step === i" :name="`step_${i}`" />
+  </div>
+</div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  components: {
+    StepperIcon: () => import('./StepperIcon.vue'),
+  },
+})
+export default class Stepper extends Vue {
+  @Prop({ type: Array, required: true })
+  public items!: string[] | { type: string, label: string }[];
+
+  @Prop({ type: Number, default: 1 })
+  public step!: number;
+
+  get itemsAsObject() {
+    return this.items.map((item) => {
+      if (typeof item === 'string') {
+        return {
+          type: 'default',
+          label: item,
+        };
+      }
+      return item;
+    });
+  }
+}
+</script>
+
+<style src="./Stepper.scss" lang="scss"></style>
---
+++ packages/mikado_reborn/src/components/Stepper/index.ts
@@ -0,0 +1,4 @@
+import MkrStepper from './Stepper.vue';
+
+export { MkrStepper };
+export default MkrStepper;
