9aa0cf8df80484b8bd20242b5469e93a71717334
Oraziolm <orazio.locchi@livementor.com>
2024-09-12 13:47:13 +0200

    refactor: Badge component

--- packages/mikado_reborn/src/components/Badge/Badge.vue
+++ packages/mikado_reborn/src/components/Badge/Badge.vue
@@ -2,32 +2,25 @@
   <span :class="`mkr__badge mkr__badge--${type}`"><slot /></span>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script lang="ts" setup>
 
-export const badgeType = [
-  'danger',
-  'danger-light',
-  'info',
-  'info-light',
-  'primary',
-  'primary-light',
-  'secondary',
-  'success',
-  'success-light',
-  'warning-light',
-  'other-1',
-];
+type badgeType =
+  'danger' |
+  'danger-light' |
+  'info' |
+  'info-light' |
+  'primary' |
+  'primary-light' |
+  'secondary' |
+  'success' |
+  'success-light' |
+  'warning-light' |
+  'other-1';
 
-export default defineComponent({
-  props: {
-    type: {
-      default: 'primary',
-      validator: (type: string) => badgeType.includes(type),
-      type: String,
-    },
-  },
-});
+const props = withDefaults(
+  defineProps<{type: badgeType}>(),
+  {type: 'primary'}
+);
 
 </script>
 
