e7e4ba5c9302b9d78497f3f5bb59cd27823f0510
Oraziolm <orazio.locchi@livementor.com>
2024-09-13 11:56:36 +0200

    Refactor: Button-TextButton component

--- packages/mikado_reborn/src/components/Button/Text/TextButton.vue
+++ packages/mikado_reborn/src/components/Button/Text/TextButton.vue
@@ -10,20 +10,13 @@
 </template>
 
 <script lang="ts" setup>
-import { PropType } from 'vue';
 import MkrButton from '../../../mixins/Button/Button.vue';
 import './TextButton.scss';
 
-type textButtonThemes = {
-  neutral: 'neutral',
-  'neutral-80': 'neutral-80',
-  'neutral-60': 'neutral-60',
-  'secondary-dark': 'secondary-dark',
-  danger: 'danger',
-};
+type textButtonThemes = 'neutral' | 'neutral-80' | 'neutral-60' | 'secondary-dark' | 'danger';
 
 const props = withDefaults(
-  defineProps<{type: PropType<keyof typeof textButtonThemes>}>(),
+  defineProps<{ type: textButtonThemes }>(),
   { type: 'neutral' }
 );
 
