c2557e316f386cf3ccf0a955ad1630b6e8b486ff
Oraziolm <orazio.locchi@livementor.com>
2024-09-18 11:34:39 +0200

    Refactor: Clean chips component

--- packages/mikado_reborn/src/components/Chips/Chips.vue
+++ packages/mikado_reborn/src/components/Chips/Chips.vue
@@ -51,62 +51,6 @@ const selectValue = () => {
   }
 };
 
-/*import { defineComponent, computed, inject, ref } from 'vue';
-import { ChipsListProvide } from './ChipsList.vue';
-import { MkrIcon } from '../Icon';
-import useUuid from '../../composables/useUuid';
-
-export default defineComponent({
-  components: {
-    MkrIcon,
-  },
-  props: {
-    label: {
-      type: String,
-      default: '',
-    },
-    value: {
-      type: String,
-      default: '',
-    },
-  },
-  setup(props: { label: string; value: string }) {
-    const { generateUUID } = useUuid();
-    const list = inject<ChipsListProvide>('list');
-    const uuid = generateUUID();
-
-    const componentId = computed(() => `chips-${uuid}`);
-    const selected = computed(() => (list ? list.value === props.value : false));
-    const chipRef = ref<HTMLElement | null>(null);
-
-    const classes = computed(() => [
-      'mkr__chips',
-      {
-        'mkr__chips--selected': selected.value,
-        'mkr__chips--small': list ? list.size === 'small' : false,
-      },
-    ]);
-
-    const selectValue = () => {
-      if (list) {
-        if (selected.value) {
-          list.emitChange('');
-        } else {
-          list.emitChange(props.value);
-        }
-      }
-    };
-
-    return {
-      uuid,
-      componentId,
-      classes,
-      selected,
-      selectValue,
-      chipRef,
-    };
-  },
-});*/
 </script>
 
 <style src="./Chips.scss" lang="scss"></style>
--- packages/mikado_reborn/src/components/Chips/ChipsList.vue
+++ packages/mikado_reborn/src/components/Chips/ChipsList.vue
@@ -47,7 +47,7 @@ const props = withDefaults(
 
 const emits = defineEmits(['input']);
 
-const chips = ref<typeof Chips[]>([]);
+// const chips = ref<typeof Chips[]>([]); // TODO: confirm useless line
 const listRef = ref<HTMLElement | null>(null);
 
 const list = reactive({
@@ -66,70 +66,6 @@ watch(() => props.value, (value: string) => {
   list.value = value;
 });
 
-/*import {
-  defineComponent, provide, reactive, ref, watch,
-} from 'vue';
-import Chips from './Chips.vue';
-
-export type ChipsListProvide = {
-  value?: string | null;
-  size: 'medium' | 'small';
-  orientation: 'row' | 'column';
-  wrap: boolean;
-  emitChange: (value: string) => void;
-  registerChips: (chips: typeof Chips) => void;
-  unregisterChips: (uuid: string) => void;
-};
-
-export default defineComponent({
-  name: 'ChipsList',
-  props: {
-    value: {
-      type: String,
-      required: true,
-      validator: (value: string | null) => typeof value === 'string' || value === null,
-    },
-    size: {
-      type: String,
-      default: 'medium',
-      validator: (size: string) => ['medium', 'small'].includes(size),
-    },
-    orientation: {
-      type: String as () => 'row' | 'column',
-      default: 'row',
-      validator: (orientation: string) => ['row', 'column'].includes(orientation),
-    },
-    wrap: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  setup(props, { emit }) {
-    const chips = ref<typeof Chips[]>([]);
-    const listRef = ref<HTMLElement | null>(null);
-
-    const list = reactive({
-      value: props.value,
-      size: props.size,
-      orientation: props.orientation,
-      wrap: props.wrap,
-      emitChange: (value: string) => {
-        emit('input', value);
-      },
-    });
-
-    provide('list', list);
-
-    watch(() => props.value, (value: string) => {
-      list.value = value;
-    });
-
-    return {
-      chips,
-      listRef,
-    };
-  },
-});*/
 </script>
 
 <style src="./ChipsList.scss" lang="scss"></style>
