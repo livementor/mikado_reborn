b04e5146030a08916bda4f02e7687c7df09241f1
Oraziolm <orazio.locchi@livementor.com>
2024-09-18 17:28:35 +0200

    Refactor: PopUp component

--- packages/mikado_reborn/src/components/PopUp/PopUp.vue
+++ packages/mikado_reborn/src/components/PopUp/PopUp.vue
@@ -9,104 +9,93 @@
   </div>
 </template>
 
-<script lang="ts">
+<script lang="ts" setup>
+import { defineProps, watch, withDefaults, ref, onMounted, nextTick } from 'vue';
 import { createPopper, Instance as PopperInstance, Placement } from '@popperjs/core';
-import {
-  defineComponent, ref, watch, onMounted, nextTick,
-} from 'vue';
 
-export default defineComponent({
-  props: {
-    placement: { type: String as () => Placement, default: 'auto' },
-    dismissable: { type: Boolean, default: false },
-    value: { type: Boolean },
-  },
-  emits: ['input'],
-  setup(props, { emit }) {
-    const popperInstance = ref<PopperInstance | null>(null);
-    const anchor = ref<HTMLElement | null>(null);
-    const content = ref<HTMLElement | null>(null);
+const props = withDefaults(
+  defineProps<{
+    placement?: Placement,
+    dismissable?: boolean,
+    value?: boolean,
+  }>(),
+  { placement: 'auto', dismissable: false }
+);
 
-    const onClickOutside = (event: MouseEvent) => {
-      const target = event.target as Node | null;
-      if (!target) {
-        return;
-      }
-      const isClickInModal = anchor.value?.contains(target) || content.value?.contains(target);
-      if (!isClickInModal) {
-        emit('input', false);
-      }
-    };
+const emit = defineEmits(['input']);
 
-    const initCloseEventListeners = () => {
-      document.addEventListener('mousedown', onClickOutside);
-    };
+const popperInstance = ref<PopperInstance | null>(null);
+const anchor = ref<HTMLElement | null>(null);
+const content = ref<HTMLElement | null>(null);
 
-    const removeCloseEventListeners = () => {
-      document.removeEventListener('mousedown', onClickOutside);
-    };
+const onClickOutside = (event: MouseEvent) => {
+  const target = event.target as Node | null;
+  if (!target) {
+    return;
+  }
+  const isClickInModal = anchor.value?.contains(target) || content.value?.contains(target);
+  if (!isClickInModal) {
+    emit('input', false);
+  }
+};
 
-    const updatePopperInstance = async (isOpened: boolean) => {
-      if (isOpened) {
-        await nextTick();
-        await popperInstance.value?.update();
-      }
-    };
+const initCloseEventListeners = () => {
+  document.addEventListener('mousedown', onClickOutside);
+};
 
-    const handleEventListeners = (isOpened: boolean) => {
-      if (props.dismissable) {
-        if (isOpened) {
-          initCloseEventListeners();
-        } else {
-          removeCloseEventListeners();
-        }
-      }
-    };
+const removeCloseEventListeners = () => {
+  document.removeEventListener('mousedown', onClickOutside);
+};
 
-    const handleButtonClick = async () => {
-      emit('input', !props.value);
-    };
+const updatePopperInstance = async (isOpened: boolean) => {
+  if (isOpened) {
+    await nextTick();
+    await popperInstance.value?.update();
+  }
+};
 
-    const handleOpening = async (isOpened: boolean) => {
-      await updatePopperInstance(isOpened);
-      handleEventListeners(isOpened);
-    };
+const handleEventListeners = (isOpened: boolean) => {
+  if (props.dismissable) {
+    if (isOpened) {
+      initCloseEventListeners();
+    } else {
+      removeCloseEventListeners();
+    }
+  }
+};
 
-    onMounted(() => {
-      if (anchor.value && content.value) {
-        const anchorChild = anchor.value.children[0] as HTMLElement;
-        const contentChild = content.value.children[0] as HTMLElement;
+const handleButtonClick = async () => {
+  emit('input', !props.value);
+};
 
-        popperInstance.value = createPopper(anchorChild, contentChild, {
-          placement: props.placement || 'bottom',
-          modifiers: [
-            {
-              name: 'offset',
-              options: {
-                offset: [0, 8],
-              },
-            },
-          ],
-        });
-      }
+const handleOpening = async (isOpened: boolean) => {
+  await updatePopperInstance(isOpened);
+  handleEventListeners(isOpened);
+};
+
+onMounted(() => {
+  if (anchor.value && content.value) {
+    const anchorChild = anchor.value.children[0] as HTMLElement;
+    const contentChild = content.value.children[0] as HTMLElement;
+
+    popperInstance.value = createPopper(anchorChild, contentChild, {
+      placement: props.placement || 'bottom',
+      modifiers: [
+        {
+          name: 'offset',
+          options: {
+            offset: [0, 8],
+          },
+        },
+      ],
     });
-
-    watch(() => props.value, (newVal) => {
-      handleOpening(newVal);
-    });
-
-    return {
-      anchor,
-      content,
-      handleButtonClick,
-      updatePopperInstance,
-      handleEventListeners,
-      initCloseEventListeners,
-      removeCloseEventListeners,
-      onClickOutside,
-    };
-  },
+  }
 });
+
+watch(() => props.value, (newVal) => {
+  handleOpening(newVal);
+});
+
 </script>
 
 <style src="./PopUp.scss" lang="scss"></style>
