dcd60d713e4bee6e82a30ffed943e6908606e7fa
Oraziolm <orazio.locchi@livementor.com>
2024-09-19 08:29:21 +0200

    Refactor: Textfield code reorder

--- packages/mikado_reborn/src/components/Textfield/Textfield.vue
+++ packages/mikado_reborn/src/components/Textfield/Textfield.vue
@@ -1,14 +1,13 @@
 <template>
-  <div :class="['mkr__textfield', { error: error }]">
+  <div :class="['mkr__textfield', { error }]">
     <div class="mkr__textfield__inner">
       <mkr-icon v-if="iconName" :color="iconColor" :name="iconName" />
       <input
-        @focus="focused=true"
-        @blur="focused=false"
-        v-bind="$attrs"
         :value="value"
         :type="getType"
         :placeholder="placeholder"
+        v-bind="$attrs"
+        @focus
         v-on="{
           ...$listeners,
           input: emitInputValue,
@@ -33,28 +32,33 @@ import { computed, ref, withDefaults, defineProps, defineEmits } from 'vue';
 import MkrIcon from '../Icon/Icon.vue';
 import MkrContainedButton from '../Button/Contained/ContainedButton.vue';
 
-const focused = ref<boolean>(false)
-const showPassword = ref<boolean>(false)
-
 const props = withDefaults(
   defineProps<{
-    value: string,
-    iconName: string,
-    placeholder: string,
-    error: boolean,
-    type: 'text' | 'email' | 'password' | 'date',
+    placeholder?: string,
+    error?: boolean,
+    type?: 'text' | 'email' | 'password' | 'date',
+    iconName?: string,
+    value?: string
   }>(),
-  { type: 'text' }
+  { type: 'text' },
 );
 
-const emit = defineEmits(['input', 'change']);
+const focused = ref(false);
+const showPassword = ref<boolean>(false);
+
+const toggleFocus = (e: Event) => {
+  e.preventDefault();
+  console.log(e);
+  if( e.type == 'focus' ) focused.value = true;
+  if( e.type == 'blur' ) focused.value = false;
+}
 
 const iconColor = computed<string>(() => focused.value ? (props.error ? 'danger' : 'secondary') : 'neutral-60');
 const getType = computed<string>(() => showPassword.value ? 'text' : props.type);
 
-const showPasswordClick = () => {
-  showPassword.value = !showPassword.value;
-};
+const emit = defineEmits(['input', 'change']);
+
+const showPasswordClick = () => { showPassword.value = !showPassword.value; };
 
 const emitInputValue = (event: InputEvent) => {
   const input = event.target as HTMLInputElement | null;
