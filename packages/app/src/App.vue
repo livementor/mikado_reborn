2396a9a6d310484ca2719caee1e51b317e1db8b9
Oraziolm <orazio.locchi@livementor.com>
2024-10-11 16:10:04 +0200

    build: finish doc-app env. : Mika'doc

--- packages/app/src/App.vue
+++ packages/app/src/App.vue
@@ -1,114 +1,86 @@
 <script setup lang="ts">
-import { MkrApp, MkrIcon, MkrStepper, MkrBadge, MkrAvatar, MkrCard, MkrCheckbox, MkrChips, MkrChipsList, MkrList, MkrInteractiveIcon, MkrListItem, MkrNavItem, MkrNavItemGroup, MkrNotificationBadge, MkrRadioGroup, MkrRadio, MkrContainedButton, MkrOutlinedButton, MkrDropdown, MkrDataTable, MkrModal, MkrExpansionPanel, MkrOverlay, MkrPopUp, MkrProgressbar, MkrSnackbar, MkrSwitch, MkrTooltip, MkrLargeTooltip, MkrTextarea, MkrSteppedProgress, MkrTextButton, MkrTextfield } from '@livementor/mikado_reborn/src/components';
+import { computed, ref, shallowRef, triggerRef } from 'vue'
+import { MkrApp } from '@livementor/mikado_reborn/src/components';
 
-import { createApp } from 'vue';
-const app = createApp({});
-app.component('MkrAvatar', MkrAvatar);
-app.component('MkrBadge', MkrBadge);
-app.component('MkrTextButton', MkrTextButton);
-app.component('MkrContainedButton', MkrContainedButton);
-app.component('MkrOutlinedButton', MkrOutlinedButton);
-app.component('MkrCard', MkrCard);
-app.component('MkrCheckbox', MkrCheckbox);
-app.component('MkrChips', MkrChips);
-app.component('MkrChipsList', MkrChipsList);
-app.component('MkrExpansionPanel', MkrExpansionPanel);
-app.component('MkrIcon', MkrIcon);
-app.component('MkrInteractiveIcon', MkrInteractiveIcon);
-app.component('MkrList', MkrList);
-app.component('MkrListItem', MkrListItem);
-app.component('MkrNavItem', MkrNavItem);
-app.component('MkrNavItemGroup', MkrNavItemGroup);
-app.component('MkrNotificationBadge', MkrNotificationBadge);
-app.component('MkrRadioGroup', MkrRadioGroup);
-app.component('MkrRadio', MkrRadio);
-app.component('MkrDropdown', MkrDropdown);
-app.component('MkrDataTable', MkrDataTable);
-app.component('MkrModal', MkrModal);
-app.component('MkrOverlay', MkrOverlay);
-app.component('MkrPopUp', MkrPopUp);
-app.component('MkrProgressbar', MkrProgressbar);
-app.component('MkrSnackbar', MkrSnackbar);
-app.component('MkrSteppedProgress', MkrSteppedProgress);
-app.component('MkrStepper', MkrStepper);
-app.component('MkrSwitch', MkrSwitch);
-app.component('MkrTooltip', MkrTooltip);
-app.component('MkrTextarea', MkrTextarea);
-app.component('MkrTextfield', MkrTextfield);
+import Avatar from '@livementor/mikado_reborn/src/components/Avatar';
+import Badge from '@livementor/mikado_reborn/src/components/Badge';
 
+/* Components lists with props and emits details */
+const componentsList = shallowRef<{ name: string, ref: string }[]>([
+  { name: 'Avatar', ref: Avatar,
+    props: [ { name: 'size', value: 5, type: 'number'}, { name: 'src', value: "https://randomuser.me/api/portraits/men/43.jpg" }, { name: 'alt', value: 'Hey man !' }, {name: 'style'}] },
+  { name: 'Badge', ref: Badge,
+    props: [ { name: 'text', value: 'Texte' }, { name: 'type', type:'select', options: ['danger', 'danger-light', 'info', 'info-light', 'primary', 'primary-light', 'secondary', 'success', 'success-light', 'warning-light', 'other-1'] }] }
+])
+
+
+/* magic */
+const activeComponentIndex = ref(1);
+const setActiveComponent = (ref: any, index: number) => {
+  activeComponentIndex.value = index;
+}
+
+const componentProps = computed(() => {
+  let props = {};
+  componentsList.value[activeComponentIndex.value].props.map(prop => {
+    props[prop.name] = prop.value;
+  })
+  return props
+})
+
+const updateShallowRef = () => triggerRef(componentsList);
 </script>
 
 <template>
-  <header>
+  <aside>
     <h1>Doc-App</h1>
-    <ul>
-      <li>/</li>
-      <li>Path</li>
-      <li>/</li>
-      <li>To</li>
-      <li>/</li>
-      <li>Component</li>
-    </ul>
-  </header>
-
-  <aside class="card-like">
-    <h3>Liste des components</h3>
-    <ul>
-      <li><i>TODO component</i></li>
-    </ul>
+    <nav class="card-like">
+      <h3>Liste des composants</h3>
+      <ul>
+        <li v-for="(component, index) in componentsList" :key="index" @click="setActiveComponent(component.ref, index)" :class="{'active': index==activeComponentIndex}">{{ component.name }}</li>
+      </ul>
+    </nav>
   </aside>
 
-  <section class="card-like">
-    <MkrApp>
-      <i>TODO</i>
+  <section>
+    <header>
+      <ul>
+        <li>/</li>
+        <li>{{ componentsList[activeComponentIndex].name }}</li>
+      </ul>
+    </header>
+
+    <MkrApp class="card-like">
+      <!-- Element Demo -->
+      <component :is="componentsList[activeComponentIndex].ref" v-bind="componentProps">{{ componentProps.text }}</component>
+      <!-- Personalized inputs -->
+      <table v-if="componentsList[activeComponentIndex].props">
+        <tr><th>Name</th><th>Value</th></tr>
+
+        <tr v-for="prop in componentsList[activeComponentIndex].props" :key="prop.name">
+          <td>{{ prop.name }}</td>
+          <!-- SELECT -->
+          <td v-if="prop.type=='select' && prop.options">
+            <select v-model="prop.value" @change="updateShallowRef">
+              <option value="" disabled>--</option>
+              <option v-for="option in prop.options" :key="option" :value="option">{{ option }}</option>
+            </select>
+          </td>
+          <!-- INPUTS -->
+          <td v-else><input :type="prop.type || 'text'" v-model="prop.value" @input="updateShallowRef"></td>
+        </tr>
+      </table>
     </MkrApp>
+
   </section>
 </template>
 
 <style scoped>
-header{
-  width: 100%;
-  padding: 10px 15px;
-  margin: 15px 15px 0;
-  display: flex;
-}
-header ul{ display: flex; list-style: none; }
-header ul li{ padding: 3px 5px; }
-
-.card-like{
-  background-color: white;
-  border: 1px solid rgba(33, 46, 59, 0.2);
-  margin: 15px;
-  padding: 30px 15px;
-  border-radius: 15px;
-}
-aside{
-  width: 300px;
-  flex: 0 0 auto;
-}
-aside h3{ margin: 0 0 15px; padding: 0; text-align: center; }
-aside ul{ margin: 0; padding:0; }
-aside ul li{
-  position: relative;
-  list-style: none;
-  padding: 3px 8px;
-}
-aside ul li::after{
-  content:'>';
-  position: absolute;
-  opacity:.3;
-  bootom: 0; right: 5px;
-  height:100%;
-  margin: auto 0 ;
-}
-aside ul li:hover{
-  background-color: #f8f8f8;
-  cursor:pointer;
-}
-
-section{
-  flex: 1 1 auto;
-  height: fit-content;
-
-}
+table{ margin: 30px 0 0; padding: 15px; width: 100%; background: #f8f8f8 }
+table th{ opacity: .8; }
+table th, table td{ text-align: start; padding: 10px 0; }
+table td:first-child{ font-weight: bold }
+table input, table select{ padding: 10px 10px; font-size: 1.8rem; border-radius: 10px; border: 1px solid lightgray }
+table input[type="text"]{ width: 600px; }
+table select{ width: 300px; }
 </style>
