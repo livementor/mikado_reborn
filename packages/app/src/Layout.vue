34a807b927034d94a0ba261c5e7baf7f534bff95
Oraziolm <orazio.locchi@livementor.com>
2024-10-16 10:00:30 +0200

    build: implementation vue-router

---
+++ packages/app/src/Layout.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { MkrApp } from '@livementor/mikado_reborn/src/components';
+
+import Avatar from '@/pages/Contexts/Avatar.vue'
+import Badge from '@/pages/Contexts/Badge.vue'
+import Button from '@/pages/Contexts/Button.vue'
+
+/* component list - aside nav list */
+const componentsList = [Avatar, Badge, Button]
+
+/* active component to display */
+const activeComponentIndex = ref(2);
+
+</script>
+
+<template>
+  <aside>
+    <h1>Mika'doc</h1>
+    <nav class="card-like">
+      <ul>
+        <RouterLink activeClass="active" to="/"><li>Accueil</li></RouterLink>
+        <RouterLink activeClass="active" to="/installation"><li>Installation</li></RouterLink>
+        <hr>
+        <RouterLink activeClass="active" to="/avatar"><li>Avatar</li></RouterLink>
+        <RouterLink activeClass="active" to="/badge"><li>Badge</li></RouterLink>
+        <RouterLink activeClass="active" to="/button"><li>Button</li></RouterLink>
+      </ul>
+    </nav>
+  </aside>
+
+  <section>
+    <header>
+      <ul>
+        <li>/</li>
+        <li>{{ componentsList[activeComponentIndex]?.__name }}</li>
+      </ul>
+    </header>
+
+    <MkrApp class="card-like">
+      <RouterView />
+    </MkrApp>
+
+  </section>
+</template>
+
+<style scoped>
+.card-like{
+  background-color: white;
+  border: 1px solid rgba(33, 46, 59, 0.2);
+  border-radius: 15px;
+}
+</style>
--- packages/app/src/main.ts
+++ packages/app/src/main.ts
@@ -1,6 +1,27 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createWebHistory, createRouter } from 'vue-router'
 
-createApp(App).mount('#app')
+import Layout from './Layout.vue'
+import Home from '@/pages/Home.vue'
+import Install from '@/pages/Install.vue'
+import Avatar from '@/pages/Contexts/Avatar.vue'
+import Badge from '@/pages/Contexts/Badge.vue'
+import Button from '@/pages/Contexts/Button.vue'
+
+const routes = [
+  { path: '/', component: Home },
+  { path: '/installation', component: Install },
+  { path: '/avatar', component: Avatar },
+  { path: '/badge', component: Badge },
+  { path: '/button', component: Button },
+]
+
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(Layout).use(router).mount('#app')
