bd40b3639432aada44de065aecb41682a14b6541
leobernier <33323385+leobernier@users.noreply.github.com>
2024-10-23 09:58:30 +0200

    refactor: add eslint ts

--- packages/app/eslint.config.js
+++ packages/app/eslint.config.js
@@ -1,13 +1,7 @@
 import pluginVue from 'eslint-plugin-vue'
+import vueTsEslintConfig from "@vue/eslint-config-typescript";
+
 export default [
-  // add more generic rulesets here, such as:
-  // js.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
-  // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
-  {
-    rules: {
-      // override/add rules settings here, such as:
-      // 'vue/no-unused-vars': 'error'
-    }
-  }
+  ...pluginVue.configs['flat/strongly-recommended'],
+  ...vueTsEslintConfig(),
 ]
